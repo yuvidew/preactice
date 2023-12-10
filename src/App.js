01898ea3fb548f67647d7e61b29b19424d02465a
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2023-12-10 15:24:31 +0530

    demo-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          this is the react app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
